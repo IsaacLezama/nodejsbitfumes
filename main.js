89f2b1d376ab991a6a69cb72a885dd893998951b
IsaacLezama <isaaclez19@gmail.com>
2023-01-31 17:25:48 -0600

    we used the events module from nodejs and we worked with the on method so we can recieve data and work with it, but we have to use the emit method to make the event work and to pass the data through it, this was just to proove how we can make us of nodejs and express together, because express is based on nodejs

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
+// installing express and creating a server
+
+// const express = require('express');
+// const app = express();
+
+// const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//     res.send('hello world');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}`);
+// })
+
+
+// event emitter with nodejs because express is based on it so we can use both in the same files
+
+const EventEmitter = require('events');
+const myEvent = new EventEmitter();
+myEvent.on('test-event', (data) => {
+    console.log('this event is listening');
+    console.log(data);
+});
+
 const express = require('express');
 const app = express();
 
 const PORT = 3000;
 
 app.get('/', (req, res) => {
+    myEvent.emit('test-event', {name: 'Isaac'});
     res.send('hello world');
 });
 
