43edc55205b6c18838009e9a0554526801dfb77f
IsaacLezama <isaaclez19@gmail.com>
2023-02-01 13:41:47 -0600

    installed mongodb and created a connection with a db in db.js file and exported, then modified the routes in the book.js file where we imported the module, then we added connection and the code for insert one new book with hardcoded data with post, and then the code for getting all the books with get

--- routes/book.js
+++ routes/book.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const bookRouter = express.Router();
 
+const connect = require('./../database/db')
+
 bookRouter.route('/')
-.get((req, res) => {
-    res.send('getting all books');
+.get(async (req, res) => {
+    const db = await connect();
+    const books = await db.collection('book').find().toArray();
+    // res.send('getting all books');
+    res.json(books);
 })
-.post((req, res) => {
+.post(async (req, res) => {
+    const db = await connect();
+    const data = {
+        title: 'Orgullo y Prejuicio',
+        author: 'Jane Austen'
+    }
+    await db.collection('book').insertOne(data);
     res.json({data: 'hello world' });
 })
 
-bookRouter.get('/:id', (req, res) => {
-    res.send('single book');
+bookRouter.route('/:id')
+.get( (req, res) => {
+    res.send('get single book');
+})
+.patch( (req, res) => {
+    res.send('single book to update');
+})
+.delete( (req, res) => {
+    res.send('single book to delete');
 });
 
 module.exports = bookRouter;
