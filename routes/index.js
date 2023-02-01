43cc2f716db0e99f4c0b3eb03680e78f7b87916d
IsaacLezama <isaaclez19@gmail.com>
2023-01-31 19:37:45 -0600

    we made use of the path module of nodejs because we need absoulte paths to work with files, we use the sendfile method where we joined the absoulte route with __dirname and the route to the file we want to show

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 const bookRouter = require('./book')
 
 router.get('/', (req, res) => {
-    res.send('hello world');
+    // res.send('hello world');
+    res.sendFile(path.join(__dirname, "/../pages/home.html"));
 });
 
 // app.get('/book', (req, res) => {
@@ -17,7 +19,8 @@ router.get('/', (req, res) => {
 router.use('/book', bookRouter);
 
 router.all('/*', (req, res) => {
-    res.send('page not found');
+    // res.send('page not found');
+    res.sendFile(path.join(__dirname, "/../pages/404.html"));
 });
 
 
