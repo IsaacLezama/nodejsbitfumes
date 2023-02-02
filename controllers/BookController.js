ff4ab49ab892be493f90173971b634081474bcd0
IsaacLezama <isaaclez19@gmail.com>
2023-02-01 18:48:12 -0600

    we created the controllers folder and added the bookcontrollerjs file, we use that file to manage the logic inside the bookjs file so the bookjs file can be only for routing

---
+++ controllers/BookController.js
@@ -0,0 +1,46 @@
+const connect = require('./../database/db');
+const { ObjectId } = require('mongodb');
+
+//la logica para el archivo de rutas book.js
+//mostrar los libros
+exports.index = async (req, res) => {
+    const db = await connect();
+    const books = await db.collection('book').find().toArray();
+    // res.send('getting all books');
+    res.json(books);
+}
+//guardar un nuevo libro
+exports.store = async (req, res) => {
+    const db = await connect();
+    // const data = {
+    //     title: 'Orgullo y Prejuicio',
+    //     author: 'Jane Austen'
+    // }
+    await db.collection('book').insertOne(req.body);
+    res.status(201).json({data: 'book inserted correctly' });
+}
+//mostrar un libro en especifico
+exports.show = async (req, res) => {
+    const _id = new ObjectId(req.params.id);
+    // console.log(_id);
+    const db = await connect();
+    const book = await db.collection('book').find({_id}).toArray();
+    res.json(book);
+    // res.send('get single book');
+}
+//modificar un libro en especifico
+exports.update = async (req, res) => {
+    const _id = new ObjectId(req.params.id);
+    const db = await connect();
+    await db.collection("book").updateOne({_id}, {$set: req.body});
+    // res.send('single book to update');
+    res.json({data: 'book updated correctly' });
+}
+//eliminar un libro en especifico
+exports.delete = async (req, res) => {
+    const _id = new ObjectId(req.params.id);
+    const db = await connect();
+    await db.collection("book").deleteOne({_id});
+    // res.send('single book to delete');
+    res.status(204).json();
+}
--- routes/book.js
+++ routes/book.js
@@ -1,48 +1,20 @@
 const express = require('express');
 const bookRouter = express.Router();
 
-const connect = require('./../database/db');
-const { ObjectId } = require('mongodb');
+// const connect = require('./../database/db');
+// const { ObjectId } = require('mongodb');
 
-bookRouter.route('/')
-.get(async (req, res) => {
-    const db = await connect();
-    const books = await db.collection('book').find().toArray();
-    // res.send('getting all books');
-    res.json(books);
-})
-.post(async (req, res) => {
-    const db = await connect();
-    // const data = {
-    //     title: 'Orgullo y Prejuicio',
-    //     author: 'Jane Austen'
-    // }
-    await db.collection('book').insertOne(req.body);
-    res.status(201).json({data: 'book inserted correctly' });
-})
-
-bookRouter.route('/:id')
-.get(async (req, res) => {
-    const _id = new ObjectId(req.params.id);
-    // console.log(_id);
-    const db = await connect();
-    const book = await db.collection('book').find({_id}).toArray();
-    res.json(book);
-    res.send('get single book');
-})
-.patch(async (req, res) => {
-    const _id = new ObjectId(req.params.id);
-    const db = await connect();
-    await db.collection("book").updateOne({_id}, {$set: req.body});
-    // res.send('single book to update');
-    res.json({data: 'book updated correctly' });
-})
-.delete(async (req, res) => {
-    const _id = new ObjectId(req.params.id);
-    const db = await connect();
-    await db.collection("book").deleteOne({_id});
-    // res.send('single book to delete');
-    res.status(204).json();
-});
+const BookController = require('./../controllers/BookController')
+//ingresar y ver libros
+bookRouter
+    .route('/')
+    .get(BookController.index)
+    .post(BookController.store)
+//mostrar, actualizar y eliminar acorde al id del libro especificado
+bookRouter
+    .route('/:id')
+    .get(BookController.show)
+    .patch(BookController.update)
+    .delete(BookController.delete);
 
 module.exports = bookRouter;
