8eafe0f8065a89c9680e156f321ceacdb76ebe7b
IsaacLezama <isaaclez19@gmail.com>
2023-02-02 19:23:06 -0600

    used the auth middleware in the books route, validations of the headers info to watch the jwt and verify if its correct and give access to the user with the correct token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,28 @@
-const auth = (req, res, next) => {
+var jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { ObjectId } = require('mongodb');
+
+const auth = async (req, res, next) => {
     // console.log('hey wazzuup this is the middlware');
     // next();
-    res.status(401).json({error: "unauthorized"});
+    if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[1])
+    {
+        const token = req.headers.authorization.split(" ")[1];
+        try {
+            const decode = await jwt.verify(token, "fake-jwt-secret");
+                const user = await User.findOne( { _id: new ObjectId(decode.user._id) });
+                
+                if(!user) {
+                    res.status(401).json({error: "unauthorized"});
+                    return
+                }
+                next();
+                // console.log(verified);
+        } catch (error) {
+            res.status(401).json({error: "unauthorized"});
+        }
+    }
+    // console.log(req.headers.authorization.split(' ')[1]);
 }
 
 module.exports = auth;
